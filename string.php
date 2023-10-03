904292c52e2a5131ad137f9bfcf9b6668a2d6052
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-10-04 01:35:43 +0600

    first commit

---
+++ string.php
@@ -0,0 +1,20 @@
+// a string is a sequence of characters like 'Hello World'.
+
+// strlen()  - Return the length of string 
+
+<?php
+    echo strlen("Hello Php");
+    // str_word_count() - count words in a string 
+    echo str_word_count("Hello PHP!!\n");
+    // string reverse 
+    echo strrev("Hello\n");
+    // Search for a text within a string 
+    echo strpos("Hello world","world",); 
+// strpos() function searches for a specific text within a string. If a match if found, the function returns the character position of the first match. If no match it will return false.
+echo "\n";
+    
+    // string replace 
+    echo str_replace("World","Dolly","Hello World"); 
+    // (which i wanna replace,what i wanna replace, in where i wanna replace);
+?>
+
